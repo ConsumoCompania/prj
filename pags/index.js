e05db876847d7935b60768538968e717d2252dd3
IngraPRJC <92542009+ConsumoCompania@users.noreply.github.com>
2021-10-14 16:55:47 -0300

    s

--- pags/index.js
+++ pags/index.js
@@ -1,6 +1,8 @@
 var p = 0
 var t = document.getElementById('b')
 var e = document.getElementById('m')
+var i = 0
+
 function menu(){
     if(p == 0){
         t.style.transform = 'translateX(100%)'
@@ -13,5 +15,29 @@ function menu(){
         e.style.paddingLeft = "0px"
         document.querySelector('main').style.width = "75%"
         document.querySelector('main').style.textAlign = "left"
+    }
+}
+
+function io(){
+    var videos = ["<iframe width='75%' height='538' src='https://www.youtube.com/embed/fj0BniP9ijk' title='YouTube video player' frameborder='0' allow=''accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"]
+    if(i==0){
+        for(var b = 0; b<videos.length; b++){
+            var n = 0
+            var art =  document.getElementById('v' + b.toString())
+            art.innerHTML =  videos[n]
+            n++
+            
+            
+        }
+        i++ 
+    }else{
+        i--
+        for(var b = 0; b<videos.length; b++){
+            var n = 0
+            var art =  document.getElementById('v' + b.toString())
+            art.innerHTML =  ''
+            n++
+        }
+
     }
 }
